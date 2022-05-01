2f682ee67ad2638044ca86ccb636d2d553cc1ea3
StienBoodts <stien.boodts@gmail.com>
2022-05-01 14:23:49 +0000

    add dom testing on the index.html file

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,8 +5,13 @@
  const buttonClick = require("../button");
 
  beforeEach(() => { //runs before each test cycle, sets up the mock DOM
-     document.body.innerHTML = "<p id='par'></p>";
- });
+    let fs = require("fs"); // node fs module: file system built into node. Allows us to open, store and read files.
+    let fileContents = fs.readFileSync("index.html", "utf-8"); // reads content of index.html using utf-8 character set and stores it into the variable 'fileContents'
+    // by default the tests run from the root directory, where index.html is
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
  
  describe("DOM tests", () => {
      test("Expects p content to change", () => {
@@ -14,4 +19,7 @@
          expect(document.getElementById("par")
              .innerHTML).toEqual("You Clicked");
      });
+     test("h1 should exist", () => {
+         expect(document.getElementsByTagName('h1').length).toBe(1); //if a h1 exist the length of the returned array will be 1, if 2 exist it would be 2...
+     })
  });
